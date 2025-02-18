f4d3cbabe977df3996c8d257fe794c4cc72182c8
Jim Smith <jimdsmith16@gmail.com>
2025-02-18 11:45:52 +0000

    Fixed Playlist.java

--- lab5-records/java/Playlist.java
+++ lab5-records/java/Playlist.java
@@ -14,15 +14,22 @@ public class Playlist {
 
     }
 
-    public static class Item {
-        public final String name;
-        public final Person performer;
+    public abstract static class Item {
         public final Float length_secs;
 
-        public Item(String name, Playlist.Person performer, Float length_secs) {
+        public Item(Float length_secs) {
+            this.length_secs = length_secs;
+        }
+    }
+
+    public static class Piece extends Item {
+        public final String name;
+        public final Person performer;
+
+        public Piece(String name, Person performer, Float length_secs) {
+            super(length_secs);
             this.name = name;
             this.performer = performer;
-            this.length_secs = length_secs;
         }
 
         @Override
@@ -31,15 +38,26 @@ public class Playlist {
         }
     }
 
-    private static Item piece1 = new Item(
+    public static class Pause extends Item {
+        public Pause(Float length_secs) {
+            super(length_secs);
+        }
+
+        @Override
+        public String toString() {
+            return "Pause (" + length_secs + ")";
+        }
+    }
+
+    private static Item piece1 = new Piece(
             "Moonlight Sonata",
             new Person("Claudio Arrau"),
             17 * 60 + 26f);
 
-    // private static Item pause1 = new Pause(5f);
+    private static Item pause1 = new Pause(5f);
 
     public static void main(String[] args) {
         System.out.println(piece1);
-        // System.out.println(pause1);
+        System.out.println(pause1);
     }
 }
